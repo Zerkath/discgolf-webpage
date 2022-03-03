bb1a7b6c66eac2ee36722162cecc5a511906ced5
Zerkath <juha.airaksinen1@gmail.com>
2022-03-03 23:15:07 +0200

    Finalized svg drawing algorithm & added webp compressed background image
    - Svg drawing for flightnumbers
      - Needs to be set to a explicit width and height to improve performance
    - Included a compressed background image with width 3440 > 1~mb
    - Sample picture for cards
    - Css changes to let text expand to card in certain screen sizes
    - dependencies..

--- templates/flightVisualization/FligthVisualizer.tsx
+++ templates/flightVisualization/FligthVisualizer.tsx
@@ -1,9 +1,13 @@
-import { ReactChild } from "react"
-import { FlightNumbers, getFlightPath } from "./svgGenerator"
+interface FlightNumbers {
+    speed: number,
+    glide: number,
+    stability: number,
+    fade: number
+}
 
 export function debugGrid() {
     return (
-        <path stroke="teal" stroke-width=".2" d={debugGridString(200)} />
+        <path stroke="grey" stroke-width=".02" d={debugGridString(20)} />
     )
 }
 
@@ -12,18 +16,6 @@ type VisualizerProps = {
     discName: string
 }
 
-
-export const FlightVisualizer = (props: VisualizerProps) => {
-    let { speed, glide, stability, fade } = props.flightNums
-    return (
-        <svg viewBox="0 0 100 200" className='flight-visualization'>
-            <text x="2" y="7" fontSize={6}>{props.discName}</text>
-            <text dx="2" y="17" fontSize={6}>{`${speed} / ${glide} / ${stability} / ${fade}`}</text>
-            <path d={getFlightPath(props.flightNums, 100, 200)} stroke="black" fill="transparent" stroke-width={1}></path>
-            {debugGrid()}
-        </ svg >
-    )
-}
 
 const debugGridString = (size: number): string => {
     let accuaracy = 20
@@ -36,5 +28,67 @@ const debugGridString = (size: number): string => {
 }
 
 const slice = (divider: number, size: number): string => {
-    return `M ${divider} 0 V${size} M 0 ${divider} H${size} `
+    // return `M ${divider} 0 V${size} M 0 ${divider} H${size} `
+    return `M 0 ${divider} H${size} `
 }
+
+function getFlightPath(flightNums: FlightNumbers, width: number, height: number): string {
+
+    let max = Math.max(width, height)
+
+    let normalized = normalizeNumbers(flightNums, max)
+    console.log(normalized)
+    let { speed, glide, stability, fade } = normalized
+
+    let h = height / (100 * (max / height))
+    let w = width / (100 * (max / width))
+
+    let middle = width / 2
+
+    let startY = height - 5 * h
+    // coordinates in x, y
+    let startCoord = `${middle.toFixed(3)}, ${startY.toFixed(3)}`
+
+    let apexY = startY - 12 * (h + speed)
+    let distance = apexY - 2.6 * (h + speed)
+
+    let stabilityOffset = w * (90 * stability - 10 * glide)
+    let apexX = middle - stabilityOffset
+
+    let apexXDiff = middle - apexX
+
+    let fadeX = apexX - (0.9 * fade + 2.2 * speed + 1.8 * stability)
+
+    let curve = `Q${middle.toFixed(3)}, ${(apexY + h * 5).toFixed(3)} ${apexX.toFixed(3)}, ${(apexY).toFixed(3)}`
+
+    var svg = `M ${startCoord} ${curve} Q${(apexX - apexXDiff).toFixed(3)}, ${(apexY - h * 5).toFixed(3)} ${fadeX.toFixed(3)}, ${(distance - h * 5).toFixed(3)}`
+
+    console.log(svg, flightNums)
+
+    return svg
+}
+
+function normalizeNumbers(flightNums: FlightNumbers, divider: number): FlightNumbers {
+    return {
+        speed: flightNums.speed / divider,
+        glide: flightNums.glide / divider,
+        stability: flightNums.stability / divider,
+        fade: flightNums.fade / divider
+    }
+}
+
+
+export const FlightVisualizer = (props: VisualizerProps) => {
+    let { speed, glide, stability, fade } = props.flightNums
+    return (
+        <svg viewBox="0 0 10 20" className='flight-visualization'>
+            <text x="0.2" y="0.8" fontSize={0.7}>{props.discName}</text>
+            <text dx="0.2" y="1.7" fontSize={0.6}>{`${speed} / ${glide} / ${stability} / ${fade}`}</text>
+            <path d={getFlightPath(props.flightNums, 10, 20)} stroke="black" fill="transparent" stroke-width={0.08}></path>
+            {debugGrid()}
+        </ svg >
+    )
+}
+
+export type { FlightNumbers }
+export { getFlightPath }

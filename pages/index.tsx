f0e90266d6b95c3ba8cb561e15d7f363935ba754
Zerkath <24889977+Zerkath@users.noreply.github.com>
2022-03-04 17:35:25 +0200

    Better wrapping for images inside cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,9 +32,8 @@ const Home: NextPage = () => {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
-            <div className='card-image'>
-              <Image src={disc} width={500}
-                height={500} alt="Placeholder alt" />
+            <div className='card-image-container'>
+              <Image src={disc} alt="Placeholder alt" className="image" />
             </div>
           </div>
 

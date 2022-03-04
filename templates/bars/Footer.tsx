26de5cc0ebf1840e22db122d1ecb8c090e45c49d
Zerkath <juha.airaksinen1@gmail.com>
2022-03-04 18:29:41 +0200

    responsive footer

--- templates/bars/Footer.tsx
+++ templates/bars/Footer.tsx
@@ -3,20 +3,26 @@ import Image from 'next/image'
 export const Footer = () => {
     return (
         <footer className='footer'>
-            <p>Contact details:</p>
-            <p>Email: example@example.com</p>
-            <p>Phone: +358 04123891152</p>
+            <div className='footer-container'>
+                <p>About us</p>
+                <div className='footer-content'>
 
-            <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by{' '}
-                <span className='logo'>
-                    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                </span>
-            </a>
+                    <p>example@example.com</p>
+                    <p>+358 04123891152</p>
+
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by{' '}
+                        <span className='logo'>
+                            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                        </span>
+
+                    </a>
+                </div>
+            </div>
         </footer >
     )
 }

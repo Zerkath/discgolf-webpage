66a1000c3241ddb9dc3ce8a2086fa5efa09cca27
Zerkath <24889977+Zerkath@users.noreply.github.com>
2022-03-09 22:45:42 +0200

    Updated content on the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,31 +13,36 @@ const Home: NextPage = () => {
       title="Home" description='Home page of Disc Golf webpage'>
       <div className='card'>
         <div className='card-text max'>
-          <h2>What is disc golf</h2>
+          <h2>Disc golf</h2>
           <p>
-            Disc golf is a sport similar to golf.
-            The aim is to get to the hole in as little shots as possible.
-            Usually played in wooded courses, but occasionally can be seen played on courses similar to regular golf courses or even sharing facilities.
+            In disc golf the goal is to get to the basket from the tee in the fewest amount of throws from the tee.
+            A fairway is usually 90m to 160m, with some exceptions. Holes typically start at par 3.
           </p>
           <p>
-            Like in golf there are discs built for different situations. Mainly split into four categories. Putters, Midranges, Fairway Drivers and Distance drivers.
-            Discs are regulated to be inside certain parameters to be allowed into competitive play.
-            Even with the regulations discs vary a lot in their flight path.
-            Discs are usually sold with 4 numbers
-            Speed, Glide, Stability and Fade.
-            These numbers are non standard and vary slightly between manufacturers,
-            but give an overall idea how a disc is going to fly.
+            In disc golf the PDGA Professional Disc Golf Association, regulates what kind of discs can be used in competitions.
+            The regulations allow for a good amount of variation.
           </p>
           <p>
-            Disc golf can be a affordable hobby, discs aren&apos;t too expensive and most courses are free to use.
-            Check out more information at
+            Discs usually land into one of the 4 slots. Putters, midranges, fairway drivers and distance drivers.
+            Putters are not built for distance, mostly for putting, but unlike golf, putting discs can still be thrown for distance.
+            They usually fly very straight with a gentle drop at the end. Great for tight fairways.
+          </p>
+          <p>
+            Midranges usually go further than putters, midranges are the largest diameter discs which mean that they retain their rotational torque the best during a flight.
+            This means the midranges keep their angle during the flight.
+          </p>
+          <p>
+            Fairways or fairway drivers are meant for distance, but with more control than distance drivers.
+          </p>
+          <p>
+            Distance drivers are meant for maximum distance, but usually for the beginners they end up being too overstable and they will get more distance with midranges.
           </p>
           <Link href="/starter">How to get started &rarr;</Link>
         </div>
       </div>
       <div className='card'>
         <div className='card-text smaller'>
-          <h2>History of the sport </h2>
+          <h2>History of the sport</h2>
 
           <p>Disc golf is a sport with origins in the early 1900s, but it really started to gain popularity around 2007.</p>
 
@@ -52,8 +57,12 @@ const Home: NextPage = () => {
         </div>
         <div className='card-text max'>
           <p>
-            Finland is one of the front runners in the sports growth with about 7700 people per one course.
-            While the United States has roughly one course for every 43000 people.
+            Disc golf has been growing for a good while, and shows no signs of slowing down.
+            In comparison to golf, disc golf is cheap to start and play. Discs cost a lot less than clubs and most courses don&apos;t cost to play.
+          </p>
+          <p>
+            The environmental impact of disc golf might be smaller when compared to regular golf, disc golf is usually played in the woods.
+            This means that courses do not convert forests into open grasslands and require less maintenance as wood courses do not fertilizer or constant lawn mowing.
           </p>
         </div>
       </div>
